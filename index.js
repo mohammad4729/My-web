5c2b0b28e9f75f8a168ed172fd07f5f4f36654e9
mohammad4729 <140237038+mohammad4729@users.noreply.github.com>
2023-12-20 18:15:27 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = 'Programming';
+var speed = 150;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
